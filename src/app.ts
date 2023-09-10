c3b9a13f1423942b7bbe7603bf7d37f9d45ed735
sabbirahmed07 <sabbirsristy@gmail.com>
2023-09-10 16:47:46 +0600

    update cords

--- src/app.ts
+++ src/app.ts
@@ -13,38 +13,38 @@ import { errorMiddleWare } from './middleware/error';
 const app: Application = express();
 
 const fileStorage = multer.diskStorage({
-	destination: './images',
-	filename: (req, file, cb) => {
-		cb(null, Date.now() + '-' + file.originalname);
-	},
+  destination: './images',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
 });
 const fileFilter = (req: any, file: any, cb: any) => {
-	if (
-		file.mimetype === 'image/png' ||
-		file.mimetype === 'image/jpg' ||
-		file.mimetype === 'image/jpeg'
-	) {
-		cb(null, true);
-	} else {
-		cb(null, false);
-	}
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 var corsOptions = {
-	origin: 'http://localhost:3000',
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: 'https://club-manager.onrender.com',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
-	multer({
-		storage: fileStorage,
-		limits: {
-			fileSize: 1024 * 1024 * 5,
-		},
-		fileFilter: fileFilter,
-	}).single('image')
+  multer({
+    storage: fileStorage,
+    limits: {
+      fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter,
+  }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, '../images')));
 
@@ -57,23 +57,23 @@ app.use('/members', memberRoutes);
 app.use(errorMiddleWare);
 
 if (process.env.NODE_ENV === 'production') {
-	//set static folder
-	app.use(express.static('client/build'));
+  //set static folder
+  app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, '../../client', 'build', 'index.html')
-		);
-	});
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
+    );
+  });
 }
 
 const port = process.env.PORT || 8080;
 
 mongoose
-	.connect(process.env.MONGO_URL as string)
-	.then((result) => {
-		app.listen(port, () => console.log('Connected'));
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(process.env.MONGO_URL as string)
+  .then((result) => {
+    app.listen(port, () => console.log('Connected'));
+  })
+  .catch((err) => {
+    console.log(err);
+  });

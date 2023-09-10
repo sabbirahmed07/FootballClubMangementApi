f95cb84907066fa5ff12579797fd7ae65f16af29
sabbirahmed07 <sabbirsristy@gmail.com>
2023-09-10 17:09:10 +0600

    removed node_env

--- src/app.ts
+++ src/app.ts
@@ -56,16 +56,14 @@ app.use('/members', memberRoutes);
 
 app.use(errorMiddleWare);
 
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//set static folder
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../../client', 'build', 'index.html')
-    );
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
+});
+// }
 
 const port = process.env.PORT || 8080;
 
